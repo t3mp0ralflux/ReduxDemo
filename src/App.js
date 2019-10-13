e0146dcff6ca977248b39137a029802fa32fa38a
t3mp0ralflux <t3mp0ralflux@gmail.com>
2019-10-13 16:19:09 -0400

    end of video post

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-        <Postform />
-        <hr />
-        <Posts />
-      </header>
-    </div>
-  );
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+            <Postform />
+            <hr />
+            <Posts />
+          </header>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
